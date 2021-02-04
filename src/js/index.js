26ec5bbaa024dc20c607e3c939b76d72194c20fa
Volodymyr <riokin2011@gmail.com>
2021-02-04 15:19:55 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 /* Here goes your JS code */
+const clickMeBtn = document.querySelector('#show-popup-form'),
+    closeBtn = document.querySelector('.popup_close'),
+    popupEl = document.querySelector('.popup')
+
+clickMeBtn.addEventListener('click', () => {
+    popupEl.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+    popupEl.style.display = ''
+})
