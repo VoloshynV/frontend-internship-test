63edf534448ca87497af50f4481812f7c12ed445
Volodymyr <riokin2011@gmail.com>
2021-02-05 12:25:13 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 /* Here goes your JS code */
 const clickMeBtn = document.querySelector('#show-popup-form'),
     closeBtn = document.querySelector('.popup_close'),
-    popupEl = document.querySelector('.popup')
+    popupEl = document.querySelector('.popup'),
+    email = document.querySelector('[name="login"]'),
+    pass = document.querySelector('[name="pass"]'),
+    submitBtn = document.querySelector('[name="submit"]'),
+    termsCheckbox = document.querySelector('[name="terms"]')
 
 clickMeBtn.addEventListener('click', () => {
     popupEl.style.display = 'block'
@@ -10,3 +14,30 @@ clickMeBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     popupEl.style.display = ''
 })
+
+formValidation(email, pass)
+
+function formValidation(email, pass) {
+    let emailIsValid = (passIsValid = checkboxIsValid = false)
+
+    email.addEventListener('input', () => {
+        const isValidEmail = validateEmail(email.value)
+        if (!isValidEmail) {
+            email.style.boxShadow = '0 0 10px red'
+        } else {
+            email.style.boxShadow = 'none'
+        }
+    })
+    pass.addEventListener('input', () => {
+        if (pass.value.length < 8 || pass.value.length > 32) {
+            pass.style.boxShadow = '0 0 10px red'
+        } else {
+            pass.style.boxShadow = 'none'
+        }
+    })
+}
+
+function validateEmail(email) {
+    const emailRegExp = /\S+@\S+\.\S+/
+    return emailRegExp.test(email)
+}
